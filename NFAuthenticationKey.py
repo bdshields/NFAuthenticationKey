c271252051e19e558a74240726bcccc3ba4a04b2
Brandon Shields <brandons@edmi.com.au>
2020-09-09 15:14:30 +1000

    The script is now in a fairly functional state

--- NFAuthenticationKey.py
+++ NFAuthenticationKey.py
@@ -1,18 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
+import sys
+import signal
 import base64
 import json
-import sys
+import random
 from datetime import datetime
 from Crypto.Cipher import AES
+
 
 
 def _pad(s):
     return s + (16 - len(s) % 16) * chr(16 - len(s) % 16)
 
 
-def processFile(filename):
-    f = open(filename)
+
+def processData():
     data = {}
     cookies = []
     cookie = dict()
@@ -23,7 +26,7 @@ def processFile(filename):
     data['timestamp']=int(datetime.now().timestamp())+432000
     state=0
     key =""
-    for line in f:
+    for line in sys.stdin:
         line = line.rstrip()
         if line == "Name":
             if state != 0:
@@ -65,41 +68,52 @@ def processFile(filename):
                 else:
                     # Tuesday, 8 September 2020 at 11:50:55
                     date = datetime.strptime(value, '%A, %d %B %Y at %H:%M:%S')
+                    if date.timestamp() < datetime.now().timestamp():
+                        print ("\n\nWARNING: Cookie data appears to have expired, log into Netflex and try again.")
                     value = date.timestamp()
             if key != "NA":
                 cookie[key]=value
             state=1
+    cookies.append(cookie)
     data['data']=dict()
     data['data']['cookies']=cookies
-    f.close()
     return data
 
 
-
-
-key=b'1234123412341234'
-
+# generate random PIN
+PIN=random.randrange(1000,9999,1)
+key="{}{}{}{}".format(PIN,PIN,PIN,PIN)
 
 iv=b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
-infile=sys.argv[1]
 
-data = processFile(infile)
+print ("Paste cookie data here, press Ctrl-D twice when finished:")
 
+
+
+data = processData()
+
+# Generate json
 json_data = json.dumps(data)
 
-print ("{}".format(json_data))
+#print ("{}".format(json_data))
 
+# initialise the cipher
 cipher = AES.new(key, AES.MODE_CBC, iv)
 
+# encrypt the data
 encoded = cipher.encrypt(_pad(json_data))
 
+# convert to base64
 output_data = base64.standard_b64encode(encoded)
 
-print ("Output: {}".format(output_data))
+# print ("Output: {}".format(output_data))
 
+# Write data to file
 f = open("NFAuthentication.Key","w")
 f.write(output_data.decode("utf-8"))
 f.close()
+
+print ("\n\nAuthentication file has been created. Your PIN is: {}".format(PIN))
 
 
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # NFAuthenticationKey
 A python script to generate the authentication key file required by CastagnaIT's Netflix Kodi plugin
+
+# Usage
+1. Log into Netflix in Google Chrome
+2. Navigate to Chrome settings, Cookies and other site data
+3. Click on "See all cookies and site data"
+4. Locate the Netflex item (netflix.com)
+5. Expand all the cookies (there should be about 6 cookies)
+6. Select the text of all the cookie data (click and drag)
+7. Copy the selected text
+8. Run the NFAuthenticationKey.py script
+9. Paste the data when prompted
+10. Press CTRL-D several time to finish the processing
+11. Should now have a key file and PIN number to use in Kodi.
+12. Note the file should be around 2.5KB
